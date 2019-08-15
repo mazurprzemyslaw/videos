c88b83fcae77d09d132fe4434d4cec22845d3ba0
Przemek Mazur <Przemek1713@gmail.com>
2019-08-15 19:12:02 +0200

    youtube api added

---
+++ src/apis/youtube.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const KEY = "AIzaSyD9913h0K1B6z6UXfOHHPALV26ZYCeFI_U";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
+
+youtube.get();
